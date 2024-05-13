2365926e37461135c57ef9ff98fff392959b2f46
Nguyen Dinh Hung <matrix16hung@gmail.com>
2024-05-14 01:53:09 +0300

    first commit

---
+++ assets/js/data.js
@@ -0,0 +1,34 @@
+export const data = [
+  {
+    id: 1,
+    link: "beat1.mp3",
+    genre: "Rap",
+    track: "Chill Dream",
+    group: "TBii Ft. UMIE",
+    year: 2021,
+  },
+  {
+    id: 2,
+    link: "beat2.mp3",
+    genre: "V-Pop",
+    track: "Muốn Bên Anh Thật Không",
+    group: "SOUTHALID",
+    year: 2022,
+  },
+  {
+    id: 3,
+    link: "beat3.mp3",
+    genre: "Rap",
+    track: "An Thần",
+    group: "(ft. Thắng) | Low G | Rap Nhà Làm",
+    year: 2021,
+  },
+  {
+    id: 4,
+    link: "beat4.mp3",
+    genre: "Rap",
+    track: "TETVOVEN",
+    group: "Wxrdie ft.AndreeRightHand",
+    year: 2023,
+  },
+];
